17da8e8a1eaf1cb7efb2885c3e5c1ce0fb8c64ba
Pat Kelly <PatK80@gmail.com>
2022-01-05 19:40:18 -0500

    Adding custom JS file

---
+++ index.js
@@ -0,0 +1,8 @@
+// let navLinks         = document.querySelectorAll(".dropdown-item");
+// let navbarCollapse   = document.querySelector(".navbar-collapse");
+// // Looping through all navLinks and adding an event listener to close navbar & change icon when clicked (made for mobile) //
+// navLinks.forEach(link => {
+//     link.addEventListener("click", () => {
+//         navbarCollapse.classList.remove("show");
+//     });
+// });
